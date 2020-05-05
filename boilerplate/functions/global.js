7abd62d661ff847c7f3e98e80753a02b69190847
ali jalali <ali.0220.jalali@gmail.com>
2020-05-06 00:39:00 +0430

    Added new function for converting query string ids to array

--- boilerplate/functions/global.js
+++ boilerplate/functions/global.js
@@ -1,7 +1,9 @@
 // Packages
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const validator = require('validator');
 // ENV
-const { PORT } = process.env;
+const { PORT, MONGO_ADDRESS } = process.env;
 // Configs
 
 // Functions
@@ -16,6 +18,17 @@ module.exports = {
         app.use(bodyParser.urlencoded({ extended: true }))
     },
     connections() {
-
+        mongoose.connect(MONGO_ADDRESS, (error) => {
+            if (error) console.log(error)
+            else console.log(`MongoDB is running successfully`)
+        })
+    },
+    convertQueryIdsToArray(ids) {
+        const result = [];
+        const array = ids.trim().split(',');
+        array.forEach((id) => {
+            if (validator.isMongoId(id.trim())) result.push(id.trim())
+        })
+        return result;
     }
 }
